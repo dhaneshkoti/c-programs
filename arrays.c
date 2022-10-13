2a8437455eaf37ad1e43dc56eda5192ff7237bc1
dhaneshkoti <chintaladhanesh@gmail.com>
2022-10-13 19:27:39 +0530

    arrays

--- arrays.c
+++ arrays.c
@@ -1,22 +1,50 @@
 #include <stdio.h>
 
-int main(){
+int arr[100], size, i,  sum = 0, option;
+void reverse(int arr[],int i);
+void printSum(int arr[],int i);
 
-    int marks[3];
 
-    printf("physics : ");
-    scanf("%d", &marks[0]);
+int main()  {
+    printf("Enter array size : ");
+    scanf("%d",&size);
 
-    printf("chemistry : ");
-    scanf("%d", &marks[1]);
+    if(size>100){
+        printf("not a valid input");
+        return 0;
+    }
+    printf("enter the array elements : \n");
+    for(i = 0; i<size;i++){
+     scanf("%d", &arr[i]);  
+    }
 
-    printf("maths : ");
-    scanf("%d", &marks[2]);
+printf("enter what you want to do \n 1.sum of elements \n2.reverse of array \n3.both");
+scanf("%d", &option);
 
-   printf("physics = %d, ", marks[0]);
-   printf("chemistry = %d, ", marks[1]);
-   printf("maths = %d, ", marks[2]);
-
-return 0;
-
+if(option == 2){
+reverse(arr,size);
+}
+else if(option == 1){
+printSum(arr,size);
+}
+else if (option == 3){
+reverse(arr,size);
+    printSum(arr,size);
+}
+else{
+    printf("wrong entry");
+}    
+    return 0;
+}
+void printSum(int arr[],int i){
+ 
+    for(i = 0; i < size; i++) {
+          sum = sum + arr[i]; }
+    
+    printf("Sum of the array is %d\n",sum);
+}
+void reverse(int arr[],int size) {
+    for(int i = size - 1; i>=0;i--){
+      printf("%d\t", arr[i]);
+    }
 }
